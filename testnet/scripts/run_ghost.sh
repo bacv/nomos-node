563935c8a0db14644852fa86efe100bd6d38b459
gusto <bacvinka@gmail.com>
2023-11-01 23:24:47 +0200

    Change run image to include older version of opensll

--- testnet/scripts/run_ghost.sh
+++ testnet/scripts/run_ghost.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-set -e
+sleep 20
+/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ hello" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
 
-exec /usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ hello" --network-config /etc/nomos/ghost-cli.yml --node-addr http://bootstrap
-exec /usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ nodes will be restarted in 10 minutes" --network-config /etc/nomos/ghost-cli.yml --node-addr http://bootstrap
-exec /usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ bye" --network-config /etc/nomos/ghost-cli.yml --node-addr http://bootstrap
+sleep 1
+/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ nodes will be restarted in 10 minutes" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+
+sleep 1
+/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ bye" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
 
