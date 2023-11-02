c2db924c4763140bd96669edca7a17a069a84457
gusto <bacvinka@gmail.com>
2023-11-02 11:27:36 +0200

    Add chat bot that loops and publishes messages

--- testnet/scripts/run_ghost.sh
+++ testnet/scripts/run_ghost.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-sleep 20
-/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ hello" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+start_time=$(date +%s)
 
-sleep 1
-/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ nodes will be restarted in 10 minutes" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+sleep 25
 
-sleep 1
-/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date) ~ bye" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+while true; do
+	/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date +%H:%M:%S) ~ hello" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
 
+	sleep 1
+	/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date +%H:%M:%S) ~ pinging" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+
+	sleep 1
+	/usr/bin/nomos-cli disseminate --user gh057-1n-7h3-5h311 --data "$(date +%H:%M:%S) ~ bye" --network-config /etc/nomos/ghost-config.yml --node-addr http://bootstrap:8080
+
+	sleep 2
+done
